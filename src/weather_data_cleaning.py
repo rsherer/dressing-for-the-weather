1ef566dfdfd0df736e6dd15aea8e437135626bb2
Robert Sherer <rsherer@gmail.com>
2018-08-28 12:35:35 -0700

    Update weather data cleaning to take in filename

--- src/weather_data_cleaning.py
+++ src/weather_data_cleaning.py
@@ -1,4 +1,8 @@
-def get_date_and_weather_from_metar(df):
+import pandas as pd
+import numpy as np
+import datetime
+
+def get_date_and_weather_from_metar(filename):
     '''
     Function to use METAR csv and convert it to a df with a day and temperature column. 
     The daily temperature is taken at 10:53a, so that it can be used for 11a, when the lunch rush starts.
@@ -7,10 +11,12 @@ def get_date_and_weather_from_metar(df):
     
     Output: pandas dataframe
     '''
+    raw_data = pd.read_csv(filename)
     mask = raw_data['valid'].apply(lambda x: x[-5:] == '10:53')
     df = raw_data[mask]
     df.columns = [x.strip().replace(' ', '_').lower() for x in df.columns]
+    df.rename(columns={'valid':'date'}, inplace=True)
     temp = df['tmpf'].apply(float)
-    date = pd.to_datetime(df['valid']).dt.date
+    date = pd.to_datetime(df['date']).dt.date
     df = pd.concat([date, temp], axis=1)
     return df
